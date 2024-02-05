29fd96b554c6fa6e3073527a433afd1dd916cbf5
Mangdi <lkw4670@naver.com>
2024-02-05 11:09:59 +0900

    feat: MainTableViewCell 파일 생성 및 기초작업

---
+++ MusicSearch/MusicSearch/Model/MainTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  MainTableViewCell.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/5/24.
+//
+
+import UIKit
+
+final class MainTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
