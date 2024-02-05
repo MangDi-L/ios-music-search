a6af76d91ac346003d21ab3c3cffed1a4c3a5db7
Mangdi <lkw4670@naver.com>
2024-02-05 14:43:08 +0900

    feat: setupMusicImageView() 구현

--- MusicSearch/MusicSearch/View/SearchResultCollectionViewCell.swift
+++ MusicSearch/MusicSearch/View/SearchResultCollectionViewCell.swift
@@ -28,6 +28,25 @@ final class SearchResultCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setupMusicImageView(urlString: String) {
+        guard let url = URL(string: urlString)  else { return }
+        
+        NetworkManager.shared.fetchImageData(url: urlString) { [weak self] result in
+            guard let self else { return }
+            switch result {
+            case .success(let data):
+                let image = UIImage(data: data)
+                // 다운로드를 시작한 순간의 url과 이미지가 다운로드 완료된 시점의 url이 동일한지를 확인해주는 코드
+                guard urlString == url.absoluteString else { return }
+                DispatchQueue.main.async {
+                    self.musicImageView.image = image
+                }
+            case .failure(let failure):
+                print("이미지 "+failure.rawValue)
+            }
+        }
+    }
+    
     private func setupAutoLayout() {
         [musicImageView, musicTitleLabel].forEach { self.addSubview($0) }
         NSLayoutConstraint.activate([
