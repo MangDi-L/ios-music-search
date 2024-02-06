364becf7557b3e9cb2d1a3b301fbeb9d731b1a02
Mangdi <lkw4670@naver.com>
2024-02-06 15:20:37 +0900

    feat: DetailViewController파일생성 및 UI추가

---
+++ MusicSearch/MusicSearch/Controller/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/6/24.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    private lazy var musicImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var musicTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var musicArtistNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var musicAlbumNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var musicReleaseDateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
