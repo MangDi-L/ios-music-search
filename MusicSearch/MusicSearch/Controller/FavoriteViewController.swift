cee5c32148648de9d26346626f389680f490e33b
Mangdi <lkw4670@naver.com>
2024-02-13 18:08:16 +0900

    feat: FavoriteViewController파일 생성

---
+++ MusicSearch/MusicSearch/Controller/FavoriteViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteViewController.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/13/24.
+//
+
+import UIKit
+
+final class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGroupedBackground
+    }
+}
