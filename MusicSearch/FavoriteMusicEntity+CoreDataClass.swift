73a036bb328912db843cb3006e213d161acd3292
Mangdi <lkw4670@naver.com>
2024-02-13 20:20:27 +0900

    feat: CoreDataModel 생성 및 NSManagedObject Subclass 생성

---
+++ MusicSearch/FavoriteMusicEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteMusicEntity+CoreDataClass.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavoriteMusicEntity)
+public class FavoriteMusicEntity: NSManagedObject {
+
+}
---
+++ MusicSearch/FavoriteMusicEntity+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  FavoriteMusicEntity+CoreDataProperties.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/13/24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension FavoriteMusicEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavoriteMusicEntity> {
+        return NSFetchRequest<FavoriteMusicEntity>(entityName: "FavoriteMusicEntity")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var artistName: String?
+    @NSManaged public var collectionName: String?
+    @NSManaged public var trackName: String?
+    @NSManaged public var imageUrl: String?
+    @NSManaged public var playTime: Double
+    @NSManaged public var releaseDateToString: String?
+
+}
+
+extension FavoriteMusicEntity : Identifiable {
+
+}
