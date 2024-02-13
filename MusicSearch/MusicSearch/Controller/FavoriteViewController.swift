209086a5bd5a171c3ff38ec85a67d701c109d3e6
Mangdi <lkw4670@naver.com>
2024-02-13 20:04:24 +0900

    feat: FavoriteViewController 테이블뷰추가 및 오토레이아웃구현

--- MusicSearch/MusicSearch/Controller/FavoriteViewController.swift
+++ MusicSearch/MusicSearch/Controller/FavoriteViewController.swift
@@ -8,9 +8,26 @@
 import UIKit
 
 final class FavoriteViewController: UIViewController {
+    private lazy var favoriteTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGroupedBackground
+        setupAutoLayout()
+    }
+    
+    private func setupAutoLayout() {
+        [favoriteTableView].forEach { view.addSubview($0) }
+        
+        NSLayoutConstraint.activate([
+            favoriteTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: UIConstants.defaultValue),
+            favoriteTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: UIConstants.defaultValue),
+            favoriteTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: UIConstants.defaultValue),
+            favoriteTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: UIConstants.defaultValue)
+        ])
     }
 }
