26a6542c5be93c4ebc3ec1470ab09a399ddd5c2f
Mangdi <lkw4670@naver.com>
2024-02-15 16:42:02 +0900

    feat: Double+Extension파일 추가 및 calculateMusicPlayTime() 구현

---
+++ MusicSearch/MusicSearch/Model/Extension/Double+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Double+Extension.swift
+//  MusicSearch
+//
+//  Created by mangdi on 2/15/24.
+//
+
+import Foundation
+
+extension Double {
+    mutating func calculateMusicPlayTime() -> String {
+        var seconds = roundl(self / TimeConstants.thousand)
+        let minutes = seconds / TimeConstants.sixty
+        seconds = Double(Int(seconds) % Int(TimeConstants.sixty))
+        if Int(seconds) < Number.ten {
+            return "\(Int(minutes)):0\(String(Int(seconds)))"
+        } else {
+            return "\(Int(minutes)):\(Int(seconds))"
+        }
+    }
+}
